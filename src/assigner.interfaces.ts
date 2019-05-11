9c25d5a28eb6543b8162103c724d681e20119e23
mernxl <mernxl@gmail.com>
2019-05-11 23:19:11 +0100

    fix(typings): use `FieldConfigTypes` on FieldConfig `type` field instead of hard-coding

--- src/assigner.interfaces.ts
+++ src/assigner.interfaces.ts
@@ -7,32 +7,41 @@ export enum FieldConfigTypes {
 }
 
 export interface DefaultFieldConfig {
-  type: 'ObjectId';
+  type: FieldConfigTypes.ObjectId;
 }
 
 export interface StringFieldConfig {
-  type: 'String';
+  type: FieldConfigTypes.String;
   nextId: string;
   separator?: string;
   nextIdFunction?: (nextId: string) => string;
 }
 
 export interface NumberFieldConfig {
-  type: 'Number';
+  type: FieldConfigTypes.Number;
   nextId: number;
   incrementBy?: number;
   nextIdFunction?: (nextId: number, incrementBy?: number) => number;
 }
 
 export interface UUIDFieldConfig {
-  type: 'UUID' | 'GUID';
+  type: FieldConfigTypes.UUID | FieldConfigTypes.GUID;
   asBinary?: boolean; // default string
   version?: 1 | 4; // supports 1 and 4, default 1
   versionOptions?: any;
 }
 
-// noSpace, insure we consume all possible values, i.e. we must have 1, 2, 3, 4
-// order doesn't matter but all those keys must be present, no 1, 3, 4, 6
+/**
+ *
+ * @property {Boolean} noSpace - noSpace, insure we consume all possible values, i.e. we must have 1, 2, 3, 4
+ * order doesn't matter but all those keys must be present, no 1, 3, 4, 6.
+ * If noSpace is true, then on holdTimeout, that nextId will be use on any newly saving doc, else nextId discarded
+ *
+ * @property {Number} maxHold[50] - As there may be performance issues when holding ids, maxHold will be set,
+ * @property {String} holdTimeout - default timeout string, must be parse-able to number by `ms` plugin
+ * @property {Number} holdTimeout - default timeout millis, gotten id stays onHold for this length of time
+ * @property {Boolean} holdTimeout - if true, will always getNextId with default timeout of `1 week` else use getOnly on getNextIds
+ */
 export type FieldConfig = {
   index?: boolean;
   unique?: boolean;
@@ -47,7 +56,7 @@ export type FieldConfig = {
  * fieldConfig = string, then nextId = string, default incrementer,
  * fieldConfig = number, then nextId = number, incrementBy = 1
  * fieldConfig = boolean(true), then fieldType = ObjectId
- * fieldConfig = GUID | UUID, then use UUID v4
+ * fieldConfig = GUID | UUID, then use UUID v1
  */
 export interface AssignerFieldsConfigMap {
   [fieldName: string]:
@@ -55,8 +64,8 @@ export interface AssignerFieldsConfigMap {
     | string
     | number
     | boolean
-    | 'GUID'
-    | 'UUID';
+    | FieldConfigTypes.GUID
+    | FieldConfigTypes.UUID;
 }
 
 /**
