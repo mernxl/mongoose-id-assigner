c3c6f4b7787196c61118307676b0f26bcbcd0462
mernxl <mernxl@gmail.com>
2020-09-25 23:34:14 +0100

    feat: package now supports uuid v8.x.x
    BREAKING CHANGE: drop support for uuid v7.x.x and less

--- src/utils/get-next-ids/get-next-id-uuid.ts
+++ src/utils/get-next-ids/get-next-id-uuid.ts
@@ -2,7 +2,9 @@ import { Binary } from 'mongodb';
 import { UUIDFieldConfig } from '../../assigner.interfaces';
 
 export function getNextIdUUID(fieldConfig: UUIDFieldConfig) {
-  const uuid = fieldConfig.version === 4 ? require('uuid/v4') : require('uuid/v1');
+  const uuidImport = require('uuid');
+
+  const uuid = fieldConfig.version === 4 ? uuidImport.v4 : uuidImport.v1;
 
   const UUID = uuid(fieldConfig.versionOptions);
 
